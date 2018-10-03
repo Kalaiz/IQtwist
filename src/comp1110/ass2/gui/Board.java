a451b75743f3a9a5789ed8465b86a95d6fb6da83
Kalaiarasan Somasundaram <u6555407@anu.edu.au>
2018-10-03 20:30:53 +1000

    Minor edit

--- src/comp1110/ass2/gui/Board.java
+++ src/comp1110/ass2/gui/Board.java
@@ -267,56 +267,8 @@ task 8 return valid_Placement()
          */
     }
 
-<<<<<<< HEAD
 
-=======
-   /* public static void main(String[] args) {
-       *//* Board b = new Board();
-        b.makeBoard();*//*
-        String str = validStartPlacement();
-        System.out.println(t.isPlacementStringValid(str));
-        System.out.println(str);
-    }*/
-    // private void makeBoard(){
-    // Random rn =new Random();
-    /*
-     *1)create an (linked)hashset of string ,such that each string is a piece placement data.
-     *  use hashsets to prevent duplicates
-     *  This hashset contains the data about those value which needs pieces to be created.
-     *  if insertion order is crucial use linked type hashset
-     *2)create a list of piece inclusive of pegs - alphas
-     *  represent pegs as with literal and 0 added behind
-     *  example: a0
-     *  Create a rng value specifically for the no of pieces to be placed
-     *  Create a string for the output
-     *3)run for loop (cond index should be greater than 1 and must us rng with a bound of x - 4 )
-     *   every iteration should check if such string exist
-     *   in the list and whether it is valid or not.
-     *  (if it exist then remove or else repeat for loop - counters updated)
-     *  then remove it from the list .After that  add it to the output string
-     *
-     *4)create a case such that Starting must at least contain a single peg
-     *  alter 4th rng to 0 just fr this case
-     *
-     *5) use rng to generate a random value
-     *      for 1st char- use list.size as reference to get the bound value
-     *                    then apply it to the list of char
-     *               2nd-  standard bound value of 8
-     *               3rd-  standard bound value of 3
-     *               4th-  standard bound value of 7
-     */
 
-//    public static void main(String[] args) {
-//       /* Board b = new Board();
-//        b.makeBoard();*/
-//        String str = valid_Placement();
-//        int i = 0;
-//        while ((t.isPlacementStringValid(str)) && i < 20){
-//            System.out.println(true);
-//            i ++;
-//        }
-//    }
->>>>>>> d72a83e1bf94dc81f8d64491f668a9f98dcd055b
 
     /*set opacity of selected pieces to a certain percentage  or
     use Blur effect for that certain piece (using setEffect) Use task 9 code for the solutions.*/
